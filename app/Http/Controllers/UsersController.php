a26e2931dbebe35311d6d3ad0217803cfbb83a46
chenlong <chenlong@styd.cn>
2018-05-02 08:51:34 +0800

    User can

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRequest;
 use App\Models\User;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
 use App\Handlers\ImageUploadHandler;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -16,11 +23,22 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+//        try {
+//            $this->authorize('update', $user);
+//        } catch (AuthorizationException $e) {
+//            return redirect()->route('login');
+//        }
+        if (Auth::user()->cant('update',$user)){
+            return redirect()->route('login');
+        }
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        if (Auth::user()->cant('update',$user)){
+            return redirect()->route('login');
+        }
         $data = $request->all();
 
         if ($request->avatar) {
